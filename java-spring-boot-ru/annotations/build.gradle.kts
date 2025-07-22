efa2d0a672e8aa920459b60ed9cf4e641810825f
@hexlet/cli <support@hexlet.io>
2025-07-22 20:21:56 +0200

    submit java-spring-boot-ru/annotations

---
+++ java-spring-boot-ru/annotations/build.gradle.kts
@@ -0,0 +1,37 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    application
+    id("com.github.ben-manes.versions") version "0.48.0"
+    id("org.springframework.boot") version "3.5.3"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "exercise"
+
+version = "1.0-SNAPSHOT"
+
+application { mainClass.set("exercise.Application") }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStandardStreams = true
+    }
+}

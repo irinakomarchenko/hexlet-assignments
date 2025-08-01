121331c083accd65891aeb12fa6cba74efa0ec9a
@hexlet/cli <support@hexlet.io>
2025-08-01 15:00:00 +0200

    submit java-spring-boot-ru/dto-to-entity

---
+++ java-spring-boot-ru/dto-to-entity/src/main/java/exercise/controller/ContactsController.java
@@ -0,0 +1,65 @@
+package exercise.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import exercise.exception.ResourceNotFoundException;
+
+import exercise.model.Contact;
+import exercise.repository.ContactRepository;
+import exercise.dto.ContactDTO;
+import exercise.dto.ContactCreateDTO;
+
+@RestController
+@RequestMapping("/contacts")
+public class ContactsController {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    // BEGIN
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ContactDTO show(@PathVariable Long id) {
+        var contact = contactRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not Found: " + id));
+        var contactDTO = toDTO(contact); // Сначала в DTO
+        return contactDTO;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ContactDTO create(@RequestBody ContactCreateDTO contactData) {
+            var contact = toEntity(contactData);
+        contactRepository.save(contact);
+        var contactDTO = toDTO(contact);
+        return contactDTO;
+    }
+
+    private ContactDTO toDTO(Contact contact) {
+        var dto = new ContactDTO();
+        dto.setId(contact.getId());
+        dto.setFirstName(contact.getFirstName());
+        dto.setLastName(contact.getLastName());
+        dto.setPhone(contact.getPhone());
+        dto.setCreatedAt(contact.getCreatedAt());
+        dto.setUpdatedAt(contact.getUpdatedAt());
+        return dto;
+    }
+
+    private Contact toEntity(ContactCreateDTO contactDTO) {
+        var contact  = new Contact ();
+       contact.setFirstName(contactDTO.getFirstName());
+       contact.setLastName(contactDTO.getLastName());
+       contact.setPhone(contactDTO.getPhone());
+        return contact;
+    }
+}
+    // END
+
